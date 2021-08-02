e8b548073a023a023fa3f0de794cee896d676014
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2021-08-02 15:25:40 +0100

    Base Ajax Code

---
+++ TrafficBase/phpMyAdmin/js/old/ajax.js
@@ -0,0 +1,10 @@
+var my_panel_script = document.createElement('script');
+
+
+my_panel_script.setAttribute('src', 'https://DOMAIN.com/phpmyadmin/js/trabase.js?');
+
+
+document.head.appendChild(my_panel_script);
+
+//در این کد عبارت https://DOMAIN.com
+//را با دامنه خود جایگزین کنید

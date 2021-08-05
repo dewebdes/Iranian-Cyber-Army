61cfb9cdd2d18d06571a131151a6f674376843cf
Eyni, Kave <40831732+dewebdes@users.noreply.github.com>
2021-08-05 04:23:18 +0100

    بارگزاری پیش نیاز های پروژه از دیتابیس

---
+++ TrafficBase/Reader/reader.py
@@ -0,0 +1,11 @@
+﻿def readlayers():
+	global gcu
+	gcu.execute("select namefull,clsname from layers;")
+	result = gcu.fetchall()
+	return list(result)
+
+def getlidx():
+	global gcu
+	gcu.execute("select idx from packets order by id desc limit 1;")
+	result = gcu.fetchall()
+	return list(result)
